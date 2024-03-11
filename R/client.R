7b7b3376e296ff2aadde6a3ce2a3beb7f0ebc96b
aitap <krylov.r00t@gmail.com>
2024-03-11 11:45:18 +0000

    client: move the connection to the state, making it possible to replace if needed

--- R/client.R
+++ R/client.R
@@ -1,7 +1,6 @@
-.makenode <- function(conn, state, index) structure(list(
-	conn = conn,
-	state = state,
-	index = index
+.makenode <- function(index, state) structure(list(
+	index = index,
+	state = state
 ), class = 'nodepool_node')
 
 # Remember the index of the node corresponding to this task
@@ -9,26 +8,26 @@ sendData.nodepool_node <- function(node, data) {
 	# serialize() has a tentency to fail without an explanation.
 	# We might avoid a timeout by select()ing first, but it's more
 	# reliable to be able to reconnect automatically.
-	socketSelect(list(node$conn), TRUE)
+	socketSelect(list(node$state$conn), TRUE)
 	if (identical(data$type, 'EXEC')) {
 		# Since the results may arrive out of order, mark every job with
 		# the index of the node it has been submitted against.
 		orig_tag <- data$data$tag
 		data$data$tag <- node$index
-		serialize(data, node$conn)
+		serialize(data, node$state$conn)
 		node$state$byindex[[node$index]] <- list(
 			tag = orig_tag,
 			complete = FALSE,
 			value = NULL
 		)
-	} else serialize(data, node$conn)
+	} else serialize(data, node$state$conn)
 }
 
 # Read one response. Look up and repair the tag. Return.
-.recvOne <- function(conn, state) {
+.recvOne <- function(state) {
 	# unserialize() will time out otherwise
-	socketSelect(list(conn))
-	value <- unserialize(conn)
+	socketSelect(list(state$conn))
+	value <- unserialize(state$conn)
 
 	stopifnot(
 		'Internal error: received a job result without a tag' = !is.null(value$tag),
@@ -45,7 +44,7 @@ sendData.nodepool_node <- function(node, data) {
 recvData.nodepool_node <- function(node) {
 	# Receive and remember responses as they come
 	while (!node$state$byindex[[node$index]]$complete) {
-		value <- .recvOne(node$conn, node$state)
+		value <- .recvOne(node$state)
 		node$state$byindex[[value$node]]$value <- value$value
 		node$state$byindex[[value$node]]$complete <- TRUE
 	}
@@ -55,7 +54,7 @@ recvData.nodepool_node <- function(node) {
 }
 
 recvOneData.nodepool_cluster <- function(cl) {
-	.recvOne(cl[[1]]$conn, cl[[1]]$state)
+	.recvOne(cl[[1]]$state)
 }
 
 stopCluster.nodepool_cluster <- function(cl, ...) {
@@ -64,7 +63,7 @@ stopCluster.nodepool_cluster <- function(cl, ...) {
 	close(cl)
 }
 
-close.nodepool_cluster <- function(con, ...) close(con[[1]]$conn)
+close.nodepool_cluster <- function(con, ...) close(con[[1]]$state$conn)
 
 .do_connect <- function(host, port) {
 	conn <- socketConnection(host, port, blocking = TRUE, open = 'a+b')
@@ -78,12 +77,10 @@ close.nodepool_cluster <- function(con, ...) close(con[[1]]$conn)
 pool_connect <- function(host, port, length = 0x80) {
 	conn <- .do_connect(host, port)
 	state <- new.env(parent = emptyenv())
+	state$conn <- conn
 	state$byindex <- vector('list', length)
 	structure(
-		lapply(
-			seq_len(length),
-			function(index) .makenode(conn, state, index)
-		),
+		lapply(seq_len(length), .makenode, state = state),
 		class = c('nodepool_cluster', 'cluster'),
 		host = host,
 		port = port
@@ -97,7 +94,7 @@ print.nodepool_cluster <- function(x, ...) {
 		if (!is.null(pid <- attr(x, 'pid'))) paste0(' (PID ', pid, ')'),
 		if ((nodes <- length(attr(x, 'nodepids'))) > 0)
 			paste(' with', nodes, 'local node[s]'),
-		if (inherits(try(isOpen(x[[1]]$conn), TRUE), 'try-error'))
+		if (inherits(try(isOpen(x[[1]]$state$conn), TRUE), 'try-error'))
 			', currently closed',
 		'>\n', sep = ''
 	)
